17c97608089182062ff4982b30d57fc7f3d24062
Prabhjot <prabhjot.singh.lamba.1@gmail.com>
2013-08-19 12:06:08 +0530

    Added bookmark.js code

---
+++ main_bookmark.js
@@ -0,0 +1,29 @@
+javascript: (function(){
+  if (!($ = window.jQuery)) {
+    script = document.createElement( "script" );  
+    script.src = "http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js";   
+    script.onload=getTheWood;  
+    document.body.appendChild(script);  
+  }   
+  else {  
+    getTheWood();  
+  }
+  
+  function getTheWood() {
+    if(!(window.VIDOOK_MAIN__)){
+      var SCRIPT_URL = "https://raw.github.com/prabhjotSL/bookmarkJS/master/main.js";
+      var jsCode = document.createElement("script");   
+      jsCode.setAttribute("src", SCRIPT_URL);
+      jsCode.onload = releasetheTrojanHorse;
+      document.body.appendChild(jsCode);
+    }
+    else {
+      releasetheTrojanHorse();
+    }
+  }
+
+  function releasetheTrojanHorse() {
+    VIDOOK_MAIN__.makeCorsRequest();
+  }
+
+})();
