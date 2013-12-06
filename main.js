ef02995d5fc4c00e3de6b1b9e6fd9590dc223009
Prabhjot Singh Lamba <prabhjot.singh.lamba.1@gmail.com>
2013-12-06 11:10:13 +0530

    Changed id to 3

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ var VIDOOK_MAIN__ = (function() {// Create the XHR object.
     var source_name = "Youtube";
     var url = location.href;
     var obj = {
-      id: 2,
+      id: 3,
       url: url,
       title: title,
       author: author,
